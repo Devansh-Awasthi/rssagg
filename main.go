e40174dca8e3f0a9df821d255fb703fb74960d43
Devansh-Awasthi <devanshawasthi03@gmail.com>
2025-08-04 09:59:30 +0530

    router

--- main.go
+++ main.go
@@ -1,6 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 func main(){
 	fmt.Println("RSS Agregator")
+	godotenv.Load(".env")
+	 PortVal := os.Getenv("PORT")
+	 if(PortVal == ""){
+		log.Fatal("port not found")
+	 }
+	 router := chi.NewRouter()
+	 router.Use(
+		cors.Handler(cors.Options{
+			AllowedOrigins: []string {"http://*","https://*"},
+			AllowedMethods: []string {"GET","POST","PUT","DELETE","OPTIONS" },
+			AllowedHeaders: []string {"*"},
+			ExposedHeaders: []string {"Link"},
+			AllowCredentials: false,
+			MaxAge: 300,
+		}))
+	 srv := &http.Server{
+		Handler: router,
+		Addr: ":"+PortVal,
+	 }
+	 fmt.Println("Port:" , PortVal)
+	 err := srv.ListenAndServe()
+	 if err != nil {
+		log.Fatal(err)
+	 }
+
 }
